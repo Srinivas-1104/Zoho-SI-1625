6cb07060ee1da2fb5c908fa52a1b175b508b2593
Srinivasan K <64210797+Srinivas-1104@users.noreply.github.com>
2022-05-16 10:36:30 +0530

    Update templates.cpp

--- Day - 08/templates.cpp
+++ Day - 08/templates.cpp
@@ -1,327 +1,421 @@
 #include <iostream>
+#include <sstream>
 #include <vector>
+#include <regex>
 #include <string>
 
 using namespace std;
 
-template <typename A>
-inline const char *get_type_name(A)
+auto compare = [](int value, string relop, int limit)
 {
-    return typeid(A).name();
-}
+    if (relop == ">=")
+        return value >= limit;
 
-class Directory
-{
-public:
-    string type;
-    string number;
-    float amount;
+    else if (relop == "<=")
+        return value <= limit;
 
-    void add_entry(string t, string no, float amt)
-    {
-        type = string(t);
-        number = no;
-        amount = amt;
-    }
+    else if (relop == ">")
+        return value > limit;
 
-    void print()
-    {
-        cout << '\t' << number << '\t' << amount;
-    }
+    else if (relop == "<")
+        return value < limit;
+
+    else if (relop == "==")
+        return value == limit;
+
+    else if (relop == "!=")
+        return value != limit;
+
+    return false;
 };
 
-vector<Directory> d;
-
-class Mobile_Connection
+class Sensors
 {
-public:
-    string number;
-    float amount;
+    public:
+        string device, relop, function;
+        int battery_level = 100;
+        bool status = true;
 
-    bool Add_Connection(string no)
-    {
-        if (no.size() != 10)
+        void on_battery_check()
         {
-            cout << "\n The phone number is not of the given size.";
-            return false;
-        }
+            cout << "\n The current battery_level: " << battery_level;
+            battery_level -= 2;
 
-        else
-        {
-            number = no;
-            amount = 0;
-            return true;
-        }
-    }
-
-    void bill_updation(int i)
-    {
-        cout << "\n Enter the amount: ";
-        cin >> amount;
-
-        d[i].amount += amount;
-    }
-
-    void bill_payment(int i)
-    {
-        cout << "\n Enter the amount: ";
-        cin >> amount;
-
-        if (amount == d[i].amount)
-        {
-            d[i].amount = 0;
-            cout << "\n The amount has been paid off.";
-            cout << "\n The current balance: " << d[i].amount;
-        }
-
-        else if (amount > d[i].amount)
-        {
-            amount -= d[i].amount;
-            d[i].amount = 0;
-            cout << "\n The excess amount is paid off: " << amount;
-        }
-
-        else
-        {
-            d[i].amount -= amount;
-            cout << "\n The remaining amount to be paid is: " << d[i].amount;
-        }
-    }
-};
-
-class LandLine_Connection
-{
-public:
-    string code;
-    string number;
-    float amount;
-
-    bool Add_Connection(string no)
-    {
-        if (no.size() < 7)
-        {
-            cout << "\n The landline number is not of the given size.";
-            return false;
-        }
-
-        else
-        {
-            int l = no.size() - 6;
-            code = no.substr(0, l);
-            number = no.substr(l);
-            amount = 0;
-            return true;
-        }
-    }
-
-    void bill_updation(int i)
-    {
-        cout << "\n Enter the amount: ";
-        cin >> amount;
-
-        d[i].amount += amount;
-    }
-
-    void bill_payment(int i)
-    {
-        cout << "\n Enter the amount: ";
-        cin >> amount;
-
-        if (amount == d[i].amount)
-        {
-            d[i].amount = 0;
-            cout << "\n The amount has been paid off.";
-            cout << "\n The current balance: " << d[i].amount;
-        }
-
-        else if (amount > d[i].amount)
-        {
-            amount -= d[i].amount;
-            d[i].amount = 0;
-            cout << "\n The excess amount is paid off: " << amount;
-        }
-
-        else
-        {
-            d[i].amount -= amount;
-            cout << "\n The remaining amount to be paid is: " << d[i].amount;
-        }
-    }
-};
-
-template <class T>
-class Bill_Processing
-{
-public:
-    T m;
-
-    bool is_number (string s)
-    {
-        for (auto it: s)
-        {
-            if (isdigit(it) == 0)
-                return false;
-        }
-
-        return true;
-    }
-
-    void add_connection()
-    {
-        string id;
-        cout << "\n Enter the number: ";
-        cin >> id;
-
-        if (is_number(id) == true)
-        {
-            if (m.Add_Connection(id) == true)
+            if (battery_level < 15)
             {
-                cout << "\n The connection has been added.";
-                Directory dir;
-                dir.add_entry(get_type_name(m), id, 0);
-                d.push_back(dir);
+                cout << "\n Low Battery: \t System Exit";
+                exit(0);
+            }
+        }
+};
+
+class Devices
+{
+    public:
+        static bool status;
+        bool door_sensor;
+        int battery_level;
+
+        void on_connect() {}
+        void on_disconnect() {}
+};
+
+class Temperature_Sensor : public Sensors
+{
+    public:
+        float temperature_limit;
+        float temperature_value = 0;
+
+        void on_temperature_change(string msg)
+        {
+            if (msg == "ti")
+                temperature_value += 1;
+
+            else if (msg == "td")
+                temperature_value -= 1;
+
+            cout << "\n The temperature value is: " << temperature_value;
+        }
+};
+
+class Lumionsity_Sensor : public Sensors
+{
+    public:
+        float brightness_limit;
+        float brightness_value = 0;
+
+        void on_brightness_change(string msg)
+        {
+            if (msg == "bi")
+                brightness_value += 1;
+
+            else if (msg == "bd")
+                brightness_value -= 1;
+
+            cout << "\n The brightness value is: " << brightness_value;
+        }
+};
+
+class Hydrolic_Sensor : public Sensors
+{
+    public:
+        float water_level_limit;
+        float water_level_value = 0;
+
+        void on_water_level_change(string msg)
+        {
+            if (msg == "wi")
+                water_level_value += 1;
+
+            else if (msg == "wd")
+                water_level_value -= 1;
+
+            cout << "\n The water_level value is: " << water_level_value;
+        }
+};
+
+class Gas_Gauge_Sensor : public Sensors
+{
+    public:
+        float gas_level_limit;
+        float gas_level_value = 0;
+
+        void on_gas_level_change(string msg)
+        {
+            if (msg == "gi")
+                gas_level_value += 1;
+
+            else if (msg == "gd")
+                gas_level_value -= 1;
+
+            cout << "\n The gas_level value is: " << gas_level_value;
+        }
+};
+
+class Proximity_Sensor : public Sensors
+{
+    public:
+        float distance_limit;
+        float distance_value = 0;
+
+        void on_distance_change(string msg)
+        {
+            if (msg == "di")
+                distance_value += 1;
+
+            else if (msg == "dd")
+                distance_value -= 1;
+
+            cout << "\n The distance value is: " << distance_value;
+        }
+};
+
+class Smart_Fan : public Devices
+{
+    public:
+        bool status;
+
+        void on_connect()
+        {
+            cout << "\n The fan is on.";
+        }
+
+        void on_disconnect()
+        {
+            cout << "\n The fan is off.";
+        }
+};
+
+class Smart_Light : public Devices
+{
+    public:
+        bool status;
+
+        void on_connect()
+        {
+            cout << "\n The light is on.";
+        }
+
+        void on_disconnect()
+        {
+            cout << "\n The light is off.";
+        }
+};
+
+class Smart_Door : public Devices
+{
+    public:
+        bool status;
+
+        void on_connect()
+        {
+            cout << "\n The door is open.";
+        }
+
+        void on_disconnect()
+        {
+            cout << "\n The door is closed.";
+        }
+};
+
+class Device_Manager : public Devices
+{
+    public:
+        string command_one, command_two, message;
+        vector<string> token_one, token_two;
+
+        Sensors s;
+
+        Temperature_Sensor t;
+        Lumionsity_Sensor l;
+        Hydrolic_Sensor h;
+        Gas_Gauge_Sensor g;
+        Proximity_Sensor d;
+
+        Smart_Fan fan;
+        Smart_Light light;
+        Smart_Door door;
+
+        void get_input()
+        {
+            s.on_battery_check();
+            cout << endl
+                << endl;
+
+            regex re(R"([\s|:,]+)");
+
+            cout << "\n The commands should be entered in the lower case::" << endl;
+
+            cout << "\n Enter the command one: ";
+            getline(cin >> ws, command_one);
+            token_one = tokenize(command_one, re);
+
+            cout << "\n Enter the command two: ";
+            getline(cin >> ws, command_two);
+            token_two = tokenize(command_two, re);
+        }
+
+        vector<string> tokenize(string str, regex re)
+        {
+            sregex_token_iterator it{str.begin(), str.end(), re, -1};
+            vector<std::string> tokenized{it, {}};
+            tokenized.erase(remove_if(tokenized.begin(), tokenized.end(),
+                                    [](string const &s)
+                                    { return s.size() == 0; }),
+                            tokenized.end());
+            return tokenized;
+        }
+
+        void remote_sensing()
+        {
+            if (token_one[1] == "temperature")
+            {
+                t.temperature_limit = stof(token_one[3]);
+                t.relop = token_one[2];
+                t.device = token_two[1];
+                t.function = token_two[2];
+            }
+
+            if (token_one[1] == "brightness")
+            {
+                l.brightness_limit = stof(token_one[3]);
+                l.relop = token_one[2];
+                l.device = token_two[1];
+                l.function = token_two[2];
+            }
+
+            if (token_one[1] == "waterlevel")
+            {
+                h.water_level_limit = stof(token_one[3]);
+                h.relop = token_one[2];
+                h.device = token_two[1];
+                h.function = token_two[2];
+            }
+
+            if (token_one[1] == "gaslevel")
+            {
+                g.gas_level_limit = stof(token_one[3]);
+                g.relop = token_one[2];
+                g.device = token_two[1];
+                g.function = token_two[2];
+            }
+
+            if (token_one[1] == "distance")
+            {
+                d.distance_limit = stof(token_one[3]);
+                d.relop = token_one[2];
+                d.device = token_two[1];
+                d.function = token_two[2];
+            }
+        }
+
+        void key_press(string key)
+        {
+            string device, function;
+            bool status;
+
+            s.on_battery_check();
+            cout << endl << endl;
+
+            if (key == "ti" || key == "td")
+            {
+                t.on_temperature_change(key);
+                status = compare(t.temperature_value, t.relop, t.temperature_limit);
+                device = t.device;
+                function = t.function;
+            }
+
+            else if (key == "bi" || key == "bd")
+            {
+                l.on_brightness_change(key);
+                status = compare(l.brightness_value, l.relop, l.brightness_limit);
+                device = l.device;
+                function = l.function;
+            }
+
+            else if (key == "wi" || key == "wd")
+            {
+                h.on_water_level_change(key);
+                status = compare(h.water_level_value, h.relop, h.water_level_limit);
+                device = h.device;
+                function = h.function;
+            }
+
+            else if (key == "gi" || key == "gd")
+            {
+                g.on_gas_level_change(key);
+                status = compare(g.gas_level_value, g.relop, g.gas_level_limit);
+                device = g.device;
+                function = g.function;
+            }
+
+            else if (key == "di" || key == "dd")
+            {
+                d.on_distance_change(key);
+                status = compare(d.distance_value, d.relop, d.distance_limit);
+                device = d.device;
+                function = d.function;
             }
 
             else
-                cout << "\n The connection has not been added.";
+                cout << "\n Invalid Key-Press Event";
+
+            carry_over(device, function, status);
         }
 
-        else
-            cout << "\n The number is invalid.";
-
-    }
-
-    void bill_updation(string number, int id)
-    {
-        m.bill_updation(id);
-    }
-
-    void bill_payment(string no, int id)
-    {
-        m.bill_payment(id);
-    }
-
-    void print_contents()
-    {
-        for (int i = 0; i < d.size(); i++)
+        void carry_over(string device, string function, bool status)
         {
-            if (d[i].type == get_type_name(m))
-                d[i].print();
+            if (device == "fan")
+            {
+                if (function == "turnon" && status == true)
+                    fan.on_connect();
+
+                else if (function == "turnoff" && status == true)
+                    fan.on_disconnect();
+
+                else if (function == "turnoff")
+                    fan.on_connect();
+
+                else
+                    fan.on_disconnect();
+            }
+
+            if (device == "light")
+            {
+                if (function == "turnon" && status == true)
+                    light.on_connect();
+
+                else if (function == "turnoff" && status == true)
+                    light.on_disconnect();
+
+                else if (function == "turnoff")
+                    light.on_connect();
+
+                else
+                    light.on_disconnect();
+            }
+
+            if (device == "door")
+            {
+                if (function == "open" && status == true)
+                    door.on_connect();
+
+                else if (function == "close" && status == true)
+                    door.on_disconnect();
+
+                else if (function == "close")
+                    door.on_connect();
+
+                else
+                    door.on_disconnect();
+            }
         }
-    }
 };
 
 int main()
 {
-
-    Bill_Processing<Mobile_Connection> mc;
-    Bill_Processing<LandLine_Connection> lc;
+    int num;
+    Device_Manager dm;
 
     do
     {
-        string value = "", number;
-        float amt;
-        int choice;
+        dm.get_input();
+        dm.remote_sensing();
 
-        cout << "\n\n";
-        cout << "\n TELECOMMUNICATIONS SYSTEM: ";
+        cout << "\n Do you wish to continue (1/0)? \t";
+        cin >> num;
+    } while (num);
 
-        cout << "\n 0. \t EXIT \n 1. \t ADD MOBILE CONNECTION \n 2. \t ADD LANDLINE CONNECTION \n 3. \t VIEW MOBILE \n 4. \t VIEW LANDLINE \n 5. \t BILL UPDATION \n 6. \t BILL PAYMENT";
+    do
+    {
+        string key;
+        cout << "\n Enter the key: ";
+        cin >> key;
+        dm.key_press(key);
 
-        cout << "\n\n Enter the choice: ";
-        cin >> choice;
-
-        switch (choice)
-        {
-        case 0:
-            cout << "\n The system is exiting.";
-            exit(0);
-
-        case 1:
-            mc.add_connection();
-            break;
-
-        case 2:
-            lc.add_connection();
-            break;
-
-        case 3:
-            mc.print_contents();
-            break;
-
-        case 4:
-            lc.print_contents();
-            break;
-
-        case 5:
-        {
-            int id;
-            cout << "\n Enter the number: ";
-            cin >> number;
-
-            for (int i = 0; i < d.size(); i++)
-            {
-                if (d[i].number == number)
-                {
-                    value = d[i].type;
-                    id = i;
-                    break;
-                }
-            }
-
-            cout << "\n Number matched.";
-
-            if (value == "")
-            {
-                cout << "\n No number match!";
-                break;
-            }
-
-            if (value == "17Mobile_Connection")
-                mc.bill_updation(number, id);
-
-            if (value == "19LandLine_Connection")
-                mc.bill_updation(number, id);
-        }
-        break;
-
-        case 6:
-        {
-            int id;
-            cout << "\n Enter the number: ";
-            cin >> number;
-
-            for (int i = 0; i < d.size(); i++)
-            {
-                if (d[i].number == number)
-                {
-                    value = d[i].type;
-                    id = i;
-                    break;
-                }
-            }
-
-            cout << "\n Number matched.";
-
-            if (value == "")
-            {
-                cout << "\n No number match!";
-                break;
-            }
-
-            if (value == "17Mobile_Connection")
-                mc.bill_payment(number, id);
-
-            if (value == "19LandLine_Connection")
-                mc.bill_payment(number, id);
-        }
-        break;
-
-        default:
-            cout << "\n Invalid Operation";
-            break;
-        }
-    } while (1);
+        cout << "\n Do you wish to continue (1/0)? \t";
+        cin >> num;
+    } while (num);
 
     return 0;
 }
